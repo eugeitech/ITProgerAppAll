9c6d845ed9e6980ccfddbc4daca3ea0f22b3375b
Euge <euge.itech@gmail.com>
2023-01-28 11:34:12 +0100

    #6  Работа с изображениями

---
+++ app/src/main/java/com/itproger/itprogerapp/image/MainImageActivity.java
@@ -0,0 +1,23 @@
+package com.itproger.itprogerapp.image;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.itproger.itprogerapp.R;
+
+public class MainImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_image);
+    }
+
+    public void goToSecondActivity(View view) {
+        Intent intent = new Intent(this, ImageSecondActivity.class);
+        startActivity(intent);
+    }
+}
