7442a71c061b58ee46f663f3b4a49d126e1a8faf
Euge <euge.itech@gmail.com>
2023-01-28 10:00:35 +0100

    #12  Обработчики действий. Отслеживание жестов

---
+++ app/src/main/java/com/itproger/itprogerapp/app/ContactsActivity.java
@@ -0,0 +1,126 @@
+package com.itproger.itprogerapp.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.itproger.itprogerapp.R;
+
+public class ContactsActivity
+        extends AppCompatActivity
+        implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
+
+    private String[] namesArray = new String[] {
+            "Joan",
+            "Josh",
+            "George",
+            "Bob",
+            "Alex",
+            "Josh",
+            "George",
+            "Bob",
+            "Alex",
+            "Josh",
+            "George",
+            "Bob",
+            "Alex"
+    };
+    private ListView listView;
+    private GestureDetectorCompat gestureDetectorCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        listView = findViewById(R.id.list_view);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.name_item,
+                R.id.user_name,
+                namesArray
+        );
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(
+                        ContactsActivity.this,
+                        (String)listView.getItemAtPosition(position),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        gestureDetectorCompat = new GestureDetectorCompat(this, this);
+        gestureDetectorCompat.setIsLongpressEnabled(true);
+        gestureDetectorCompat.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    public void goHome(View view) {
+        Intent intent = new Intent(this, MainAccountActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onDown(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(@NonNull MotionEvent e) {
+        Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(@NonNull MotionEvent e) {
+        return false;
+    }
+}
